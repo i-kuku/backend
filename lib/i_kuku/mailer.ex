ab0dbbf66e50b561afbe4e88a618b0b5fff505b7
Velma <akinyivee10@gmail.com>
2023-11-20 10:08:13 +0300

    credo and config setup

--- lib/i_kuku/mailer.ex
+++ lib/i_kuku/mailer.ex
@@ -1,3 +1,5 @@
 defmodule IKuku.Mailer do
+  @moduledoc false
+
   use Swoosh.Mailer, otp_app: :i_kuku
 end
--- lib/i_kuku_web/telemetry.ex
+++ lib/i_kuku_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule IKukuWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 
